66794cc173dfd2e227ffa28e877a2d9120c6fba9
daanial11 <daanial74@gmail.com>
2022-03-29 16:30:03 +0100

    a_star

--- controllers/bot_controller/bot_controller.py
+++ controllers/bot_controller/bot_controller.py
@@ -2,23 +2,41 @@
 
 # You may need to import some classes of the controller module. Ex:
 #  from controller import Robot, Motor, DistanceSensor
+from re import S
 from webots.controller import Supervisor, Emitter
 import struct
-
+from a_star import a_star
 
 
 num_epucks = 2
 
-start_cord = (-1.38, -0.875)
+origin = (-1.4, -0.875)
+square_dist = 0.25
 epucks = []
 
+map_squares = []
+
+for x in range(12):
+    for y in range(8):
+        cords = (origin[0] + (square_dist * x), origin[1] + (square_dist * y))
+        map_squares.append(cords)
 
 
-# create the Robot instance.
+
+
+# create the Robot instance
 supervisor = Supervisor()
 
 emitter = Emitter(name="robot")
 
+
+
+start = (origin[0], origin[1])
+end = (origin[0] + (6 * square_dist), origin[1] + (3 * square_dist))
+
+path = a_star(map_squares, start, end, origin, square_dist)
+
+print(path)
 
 for i in range(num_epucks):
     epucks.append(supervisor.getFromDef(f"EPUCK{i}"))
@@ -26,8 +44,10 @@ for i in range(num_epucks):
 emitter.setChannel(channel=-1)
 emitter.setRange(range=-1)
 
-epucks[0].getField("translation").setSFVec3f([start_cord[0], start_cord[1], 0])
-epucks[1].getField("translation").setSFVec3f([start_cord[0] + (6 * 0.25), start_cord[1] + (3 * 0.25), 0])
+
+
+#epucks[0].getField("translation").setSFVec3f([start_cord[0], start_cord[1], 0])
+#epucks[1].getField("translation").setSFVec3f([start_cord[0] + (6 * 0.25), start_cord[1] + (3 * 0.25), 0])
 
 # get the time step of the current world.
 timestep = int(supervisor.getBasicTimeStep())
