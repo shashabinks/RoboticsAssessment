210498a5cb907df4d76e76fa503db09ef01ad4e4
daanial11 <daanial74@gmail.com>
2022-04-28 17:14:12 +0100

    basic 8x8

--- results/stats.py
+++ results/stats.py
@@ -3,28 +3,35 @@ from unittest import result
 
 
 
-with open('Adv_algo/results_epucks6_basic.json', 'r') as f:
-    data = json.load(f)
 
 
-with open('basic_algo/results_epucks6_basic.json', 'r') as f:
-    data1 = json.load(f)
+def collect_results(metric, path, num_epucks):
+    with open(path, 'r') as f:
+        data = json.load(f)
 
 
-adv_coll_count = 0
-adv_path_count = 0
-basic_coll_count = 0
-basic_path_count = 0
+    metric_count = 0
 
-for iteration in data['results']:
-    adv_coll_count+= iteration['collision']
-    adv_path_count+= iteration['paths completed']
+    for iteration in data['results']:
+        if iteration['iteration'] > 500:
+            break
+        metric_count+= iteration[metric]
+        
+    if metric == "collision":
+        print(metric_count)
+
+    if metric == "paths completed":
+        print(f" path completed:{((metric_count/500)/num_epucks) * 100} %")
+
+    if metric == "average path completed time":
+        print(f" average time: {metric_count/500}")
 
 
-for iteration in data1['results']:
-    basic_coll_count+= iteration['collision']
-    basic_path_count+= iteration['paths completed']
+path = "../final_results/8x4_map_size_basic_algo/results_epucks6.json"
 
 
-print(f"adv algo coll: {adv_coll_count}" + f" path completed %: {((adv_path_count/100)/6) * 100}%")
-print(f"basic algo coll: {basic_coll_count}" + f" path completed %: {((basic_path_count/100)/6) * 100}%")
+collect_results("collision", path, 6)
+
+
+
+
